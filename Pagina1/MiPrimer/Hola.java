9c244ebc6adfc222f948a8620f45629553618f0a
Dancerd12 <82984947+Dancerd12@users.noreply.github.com>
2021-04-21 19:49:12 -0500

    Cambios

--- Pagina1/MiPrimer/Hola.java
+++ Pagina1/MiPrimer/Hola.java
@@ -3,6 +3,9 @@ package Pagina1.MiPrimer;
 public class Hola {
     public static void main(String[]args){
         System.out.println("Funciono dani");
+        System.out.println("Funciono dani1");
+        System.out.println("Funciono dani2");
+        System.out.println("Funciono dani3");
     }
     
 }
